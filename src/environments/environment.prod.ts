f3f429623a631c2ee5a8472ccc6c4abc8c834f5d
Gert Paimla <gertpaimla@gmail.com>
2020-11-05 14:59:16 +0200

    Update src/environments/environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,10 @@ export const environment = {
   apiHost: 'REST_API_URL_REPLACE',
   apiNLG: 'NLG_API_URL_REPLACE',
   apiBasePath: '',
+  apiHostTK: 'EMBEDDIA_TEXTA_HOST',
+  apiBasePathTK: '/api/v1',
+  projectId: 'EMBEDDIA_TEXTA_PROJECT_ID',
+  toolkitUsername: 'EMBEDDIA_TEXTA_USERNAME',
+  toolkitPass: 'EMBEDDIA_TEXTA_PASSWORD',
   production: true
 };
